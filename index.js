eeca011e05070f3daf4efd3a997bd9d389926d7d
Joseph Martre <martre.joseph@gmail.com>
2025-03-17 15:04:46 +0100

    Replaced string concat with string interpolation

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ import { html, render, useState } from "htm/preact";
  * @returns {string}
  */
 const capitalizeFirstLetter = (string) => {
-  return string ? string[0].toUpperCase() + string.slice(1) : string;
+  if (string.length === 0) return string;
+  return `${string[0].toUpperCase()}${string.slice(1)}`;
 };
 
 /**
